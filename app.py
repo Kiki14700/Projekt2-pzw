a7ad08b0083ef5517c151b5f2e9999229bd338cb
Kiki14700 <kknezevic147@gmail.com>
2021-02-05 19:44:57 +0100

    projekt 2

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_caching import Cache
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
 import requests
-
 
 
 app = Flask(__name__)
 
 app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'united' 
-
+app.config['SECRET_KEY']='LongAndRandomSecretKey'
 bootstrap = Bootstrap(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    username = StringField(label=('Unesite ime:'), validators=[DataRequired()])
+    submit = SubmitField(label=('Submit'))
+    surname = StringField(label=('Unesite prezime:'), validators=[DataRequired()])
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -21,9 +28,18 @@ def index():
 def proizvodi():
     return render_template('proizvodi.html')
 
-@app.route('/cijenik')
-def cijenik():
-    return render_template('cijenik.html')
+@app.route('/cjenik')
+def cjenik():
+    return render_template('cjenik.html')
+
+@app.route('/', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    username = form.username.data
+    surname = form.surname.data
+    if form.validate_on_submit():
+           return render_template('index.html', username = username, surname = surname)
+    return render_template('login.html', form=form)
 
 @app.route('/tečaj')
 @cache.cached(timeout=60)
